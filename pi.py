608f3b84821bab4e03f1cb434ea526f57876c13e
Alyssa Margalit <alyssam0@usc.edu>
2020-10-31 16:35:57 -0700

    s

--- pi.py
+++ pi.py
@@ -6,6 +6,14 @@ from grovepi import *
 import math
 import paho.mqtt.client as mqtt
 import RPi.GPIO as GPIO
+import time
+
+servoPIN = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
+
+p = GPIO.PWM(servoPIN, 50)
+p.start(2.5)
 
 
 
