87d6b54a009781b7119c55625716b05de0d11eed
Frida Hill <fhill@usc.edu>
2020-10-31 20:44:30 -0700

    test

--- buzzer.py
+++ buzzer.py
@@ -5,14 +5,24 @@ buzzer = 6
 
 grovepi.pinMode(buzzer, "OUTPUT")
 
+def variable_delay_us(delay):
+	int i = (delay + 5)/10
+	while i != 0:
+		time.sleep(0.1)
+
 def play_note(freq):
 
 	period = 1000/freq
-	while True:
+	while freq != 0:
 		grovepi.analogWrite(buzzer, 1)
-		time.sleep(1)
+		variable_delay_us(period/2)
 		grovepi.analogWrite(buzzer, 0)
-		time.sleep(1)
+		variable_delay_us(period/2)
 		freq = freq - 1
 
+
 play_note(440)
+
+except KeyboardInterrupt: 
+	grovepi.digitalWrite(buzzer, 0)
+	break
