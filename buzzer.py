2f391e7632b690b98433483d11ade70b84c71b3f
Alyssa Margalit <alyssam0@usc.edu>
2020-10-31 20:30:04 -0700

    s

--- buzzer.py
+++ buzzer.py
@@ -4,15 +4,16 @@ import grovepi
 buzzer = 6
 
 grovepi.pinMode(buzzer, "OUTPUT")
+usleep = lambda x: time.sleep(x/1000000.0)
 
 def play_note(freq):
 
 	period = 1000/freq
 	while True:
 		grovepi.analogWrite(buzzer, 1)
-		time.sleep(1)
+		usleep(1700)
 		grovepi.analogWrite(buzzer, 0)
-		time.sleep(1)
-		freq = freq - 1
+		time.sleep(1700)
+		
 
 play_note(440)
