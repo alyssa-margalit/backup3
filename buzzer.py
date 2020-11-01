c53c5690baa8359de39e9a52a5daa91911c07176
Frida Hill <fhill@usc.edu>
2020-10-31 20:15:58 -0700

    test

--- buzzer.py
+++ buzzer.py
@@ -8,11 +8,11 @@ grovepi.pinMode(buzzer, "OUTPUT")
 def play_note(freq):
 
 	period = 1000/freq
-	while freq = freq - 1:
+	while True:
 		grovepi.digitalWrite(buzzer, 1)
 		time.sleep(1)
 		grovepi.digitalWrite(buzzer, 0)
 		time.sleep(1)
+		freq = freq - 1
 
-while True:
-	play_note(440)
+play_note(440)
