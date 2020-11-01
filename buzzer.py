a410701b56270d0026890438edb0bd89ebc842a2
Frida Hill <fhill@usc.edu>
2020-10-31 17:07:13 -0700

    test

---
+++ buzzer.py
@@ -0,0 +1,23 @@
+import time 
+import grovepi
+
+buzzer = 8
+
+grovepi.pinMode(buzzer, "OUTPUT")
+
+while True:
+	try:
+		grovepi.digitalWrite(buzzer, 1)
+		print('start')
+		time.sleep(1)
+
+		grovepi.digitalWrite(buzzer, 0)
+		print("stop")
+		time.sleep(1)
+
+	except KeyboardInterrupt:
+		grovepi.digitalWrite(buzzer, 0)
+		break
+
+	except IOError:
+		print("error")
