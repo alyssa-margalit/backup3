f68175b5393528fa6299d82788147836a20cfcee
Alyssa Margalit <alyssam0@usc.edu>
2020-11-03 15:32:53 -0800

    s

--- sound.py
+++ sound.py
@@ -1,15 +1,3 @@
-import pygame
-import time
-import sys
-import grovepi
-from grove_rgb_lcd import *
-from grovepi import *
-import math
-import paho.mqtt.client as mqtt
-import RPi.GPIO as GPIO
-import time
-from pygame import mixer
-mixer.init()
-mixer.music.load("forest.mp3")
-mixer.music.play()
-setText("hello")
+import playsound
+playsound.playsound('forest.mp3', False)
+print ('...')
