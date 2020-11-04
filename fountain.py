993a37166e80ce10b757a79bf806c023f389db61
Alyssa Margalit <alyssam0@usc.edu>
2020-11-04 13:42:32 -0800

    s

--- fountain.py
+++ fountain.py
@@ -4,6 +4,6 @@ GPIO.setmode(GPIO.BCM)            # choose BCM or BOARD
 GPIO.setup(4, GPIO.OUT) # set a port/pin as an output  
 while True: 
 	GPIO.output(4, 1)       # set port/pin value to 1/GPIO.HIGH/True
-	time.sleep(1)
+	time.sleep(10)
 	GPIO.output(4, 0)       # set port/pin value to 0/GPIO.LOW/False  
-	time.sleep(1)
+	time.sleep(0)
