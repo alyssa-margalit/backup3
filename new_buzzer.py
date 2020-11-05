ad92d6e2b47bf67bd460b8010d2a377efb31d7ba
Frida Hill <fhill@usc.edu>
2020-11-04 22:28:10 -0800

    test

--- new_buzzer.py
+++ new_buzzer.py
@@ -2,46 +2,17 @@ import time
 import grovepi
 
 buzzer = 3
- 
-length = 15
-notes = ["c", "c", "g", "g", "a", "a", "g", "f", "f", "e", "e", "d", "d", "c"]
-beats = [1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 4]
-tempo = 300;
+freq = 440
+i = 0
 
-def setup():
-	grovepi.pinMode(buzzer,"OUTPUT")
-
-
-def loop():
-
-	for i in range(length):
-		if notes[i] == ' ':
-			delay(beats[i] * tempo)
-		else:
-			playNote(notes[i], beats[i] * tempo)
-			delay(tempo / 2)
+while True:
+	try: 
+		if i < freq:
+			grovepi.analogWrite(buzzer, i)
+			i = i + 20
+			time.sleep(0.5)
 
 
-
-def playTone(tone, duration):
-	step = 0
-	end = duration * 1000
-	for i in range (0, step, end):
-		grovepi.digitalWrite(buzzer, 1)
-		time.sleep(tone/100000)
-		grovepi.digitalWrite(buzzer, 0)
-		time.sleep(tone/100000)
-		i += tone*2
-		step = i
-
-
-def playNote(note, duration) :
-	char_names = ["c", "d", "e", "f", "g", "a", "b", "C"]
-	tones = [1915, 1700, 1519, 1432, 1275, 1136, 1014, 956]
-	step = 1
-	for i in range(0, step, 8):
-		if names[i] == note:
-			playTone(tones[i], duration);
-			step += 1
-
-playNote(c, 1)
+	except KeyboardInterrupt:
+		grovepi.analogWrite(buzzer,0)
+		break
