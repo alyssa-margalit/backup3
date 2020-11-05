dba4f753c285f8adfd1cfc6fbf1ce675b1be5fd0
Frida Hill <fhill@usc.edu>
2020-11-04 17:26:12 -0800

    test

--- new_buzzer.py
+++ new_buzzer.py
@@ -13,9 +13,9 @@ def variable_delay_us(delay):
 def play_note(freq):
 	period = 1000000/freq
 	while freq != 0:
-		grovepi.digitalWrite(buzzer,1)
+		grovepi.analogWrite(buzzer,1)
 		variable_delay_us(period/2)
-		grovepi.digitalWrite(buzzer,0)
+		grovepi.analoglWrite(buzzer,0)
 		variable_delay_us(period/2)
 
 play_note(262)
