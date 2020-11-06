33a2986595f0bf1a97a4cdac66a1c2a48ddef2a3
Alyssa Margalit <alyssam0@usc.edu>
2020-11-06 12:38:58 -0800

    s

--- test2.py
+++ test2.py
@@ -1,22 +1,24 @@
 import time
 import grovepi
 from grovepi import *
-
+import math
 
 
 potentiometer = 1
 button = 4
 pinMode(button ,"INPUT")
 
-time.sleep(1)
-pot = 0
+#time.sleep(1)
+#pot = 0
 while True:
 	try: 
+		print("here")
 		pot = grovepi.analogRead(potentiometer)
 		print(pot)
 		status = digitalRead(button)
 		if status:
 			print("pressed")
+		time.sleep(1)
 
 	except IOError:
 		print("fail")
