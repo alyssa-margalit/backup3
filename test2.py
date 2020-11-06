0e5ab959c89858652ca574bf2ba526715ff23dc0
Alyssa Margalit <alyssam0@usc.edu>
2020-11-06 12:27:06 -0800

    s

--- test2.py
+++ test2.py
@@ -21,29 +21,10 @@ potentiometer = 2
 servoPIN = 17
 fountain = 27
 
-#set up buzzer and button
-grovepi.pinMode(buzzer, "OUTPUT")
-grovepi.pinMode(button, "INPUT")
-
-#set up the motor
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN, GPIO.OUT)
-p = GPIO.PWM(servoPIN, 50)
-p.start(2.5)
-
-#set up the fountain
-GPIO.setup(fountain, GPIO.OUT) # set a port/pin as an output  
-GPIO.output(fountain, 1)       # set port/pin value to 1/GPIO.HIGH/True
-
-#set up the LEDs
-GPIO.setup(red_led, GPIO.OUT) # set a port/pin as an output  
-GPIO.output(red_led, 0)       # set port/pin value to 1/GPIO.HIGH/True
-GPIO.setup(green_led, GPIO.OUT) # set a port/pin as an output  
-GPIO.output(green_led, 1)       # set port/pin value to 1/GPIO.HIGH/True
-GPIO.setup(blue_led, GPIO.OUT) # set a port/pin as an output  
-GPIO.output(blue_led, 0)       # set port/pin value to 1/GPIO.HIGH/True
-
-print("pin initialization complete")
-
-
-
+while True:
+	try:
+		pot = grovepi.analogRead(potentiometer)
+		print(pot)
+		time.sleep(1)
+	else:
+		print("fail")
