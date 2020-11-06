6b1ccd0a62f621f349a71b5425d47cc99d583ca7
Alyssa Margalit <alyssam0@usc.edu>
2020-11-06 12:29:56 -0800

    s

--- test2.py
+++ test2.py
@@ -1,30 +1,13 @@
 import time
-import sys
 import grovepi
-from grove_rgb_lcd import *
-from grovepi import *
-import math
-import paho.mqtt.client as mqtt
-import RPi.GPIO as GPIO
-import time
 
+potentiometer = 1
 
-GPIO.setwarnings(False)
-
-red_led = 2
-green_led = 3
-blue_led = 4
-button = 4
-ranger = 3
-buzzer = 2
-potentiometer = 2
-servoPIN = 17
-fountain = 27
-
+time.sleep(1)
+pot = 0
 while True:
-	try:
+	try: 
 		pot = grovepi.analogRead(potentiometer)
 		print(pot)
-		time.sleep(1)
 	except IOError:
 		print("fail")
